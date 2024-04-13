4a2681dd9462bade085e7befa907403807a4e22d
MD SALIM HOSSAIN <159735559+mdsalimhossain40476@users.noreply.github.com>
2024-04-13 21:40:28 +0600

    Condition Operator
    C# Comparison Operators
                     Comparison operators দুটি মান (বা ভেরিয়েবল) তুলনা করতে ব্যবহৃত হয়। 
                     এটি প্রোগ্রামিংয়ে গুরুত্বপূর্ণ, কারণ এটি আমাদের উত্তর খুঁজে পেতে এবং সিদ্ধান্ত নিতে সাহায্য করে।
    
                     একটি Comparison রিটার্ন মান হল True বা False। এই মানগুলিকে বুলিয়ান মান 
                     হিসাবে পরিচিত করা হয় এবং বুলিয়ান এবং If..Else অধ্যায়ে আপনি এগুলি সম্পর্কে আরও জানতে পারবেন।
    
                    C# Logical Operators
                    Comparison অপারেটরের মতোই, আপনি Logical অপারেটরের সাহায্যেও সত্য বা মিথ্যা মানের জন্য পরীক্ষা করতে পারেন।
                   
                    লজিক্যাল অপারেটরগুলি চলক বা মানগুলির মধ্যে লজিক নির্ধারণ করতে ব্যবহৃত হয়:

--- B-A C#/06 Conditional Operator/Program.cs
+++ B-A C#/06 Conditional Operator/Program.cs
@@ -92,6 +92,27 @@ namespace ConditionalOperator
             {
                 WriteLine("Reverse the result, returns False if the result is true");
             } //  যেহেতু উভয় বিবৃতি False তাই Reverse the result, returns False if the result is true রিটার্ন করে
+                        /*
+                            C# Short Hand If...Else
+                            Short Hand If...Else (Ternary Operator)
+                            একটি সংক্ষেপ আছে, যা টারনারি অপারেটর হিসেবে পরিচিত কারণ এটি তিনটি অপারেন্ডে গঠিত। এটি একাধিক লাইনের কোডকে একটি লাইনে প্রতিস্থাপন করতে ব্যবহার করা যেতে পারে। এটি প্রায়ই সহজ If...Else স্টেটমেন্ট প্রতিস্থাপন করতে ব্যবহার করা হয়
+                            variable = (condition) ? expressionTrue :  expressionFalse;
+                        */
+                        // লেখার পরিবর্তে।
+                         int time = 20;
+                          if (time < 18) 
+                          {
+                            Console.WriteLine ("Good day.");
+                          } 
+                          else 
+                          {
+                            Console.WriteLine ("Good evening.");  //  Output : Good evening.
+                          }
+                          // আপনি সহজেই লিখতে পারেন।
+                          int time = 20;
+                          string result = (time < 18) ? "Good day." : "Good evening.";
+                          Console.WriteLine (result);   //  Output : Good evening.
+                                
             ReadLine();
         }
     }
